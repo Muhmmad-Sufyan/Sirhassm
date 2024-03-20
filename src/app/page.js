7081790bc78b4ba4ad0f29e999dc4155f852eea9
Muhmmad-Sufyan <144341878+Muhmmad-Sufyan@users.noreply.github.com>
2024-03-20 13:07:27 +0500

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,24 @@
+import HeroSection from '../components/HeroSection'
+import FeatureCourses from '../components/FeatureCourses'
+import WhyChooseUs from '../components/WhyChooseUs'
+import EhsaslabCard from '../components/EhsaslabCard'
+import UpcomingWebnar from '../components/UpcomingWebnar'
+import OurTeam from '../components/OurTeam'
+export default function Home() {
+  return (
+
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+
+      <HeroSection />
+      <FeatureCourses />
+      <WhyChooseUs />
+      <EhsaslabCard />
+      <UpcomingWebnar />
+    <OurTeam />
+
+    </main>
+
+
+
+  )
+}
